e1fb27d632b36443b3e6c081921ec4f576a2dcf9
DavronbekMamarasulov07 <nilufar12345davron@icloud.com>
2024-10-29 16:41:51 +0500

    first commit

---
+++ src/components/installing/Installing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Installing = () => {
+  return (
+    <div>Installing</div>
+  )
+}
+
+export default Installing
---
+++ src/db/index.js
@@ -0,0 +1,243 @@
+const products = [
+    {
+        id: 1,
+        name: "50 WC. Одноконтурный. Настенный",
+        imageUrl: "../images/product1.png",
+        price: 5000
+    },
+    {
+        id: 2,
+        name: "30 WC. Одноконтурный. Настенный",
+        imageUrl: "../images/product2.png",
+        price: 3000
+    },
+    {
+        id: 3,
+        name: "50 WC. Одноконтурный. Настенный",
+        imageUrl: "../images/product1.png",
+        price: 5000
+    },
+    {
+        id: 4,
+        name: "35 WC. Одноконтурный. Настенный",
+        imageUrl: "../images/product4.png",
+        price: 3000
+    },
+    {
+        id: 5,
+        name: "30 WC. Одноконтурный. Настенный",
+        imageUrl: "../images/product4.png",
+        price: 3000
+    },
+    {
+        id: 6,
+        name: "24 WC. Одноконтурный. Настенный",
+        imageUrl: "../images/product4.png",
+        price: 2500
+    },
+    {
+        id: 7,
+        name: "24 WC. Одноконтурный. Настенный",
+        imageUrl: "../images/product4.png",
+        price: 3000
+    },
+    {
+        id: 8,
+        name: "18 WC. Одноконтурный. Настенный",
+        imageUrl: "../images/product4.png",
+        price: 2500
+    },
+    {
+        id: 9,
+        name: "50 DС. Двухконтурный настенный",
+        imageUrl: "../images/product4.png",
+        price: 5000
+    },
+    {
+        id: 10,
+        name: "40 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product1.png",
+        price: 5000
+    },
+    {
+        id: 11,
+        name: "35 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product4.png",
+        price: 3000
+    },
+    {
+        id: 12,
+        name: "30 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product4.png",
+        price: 3500
+    },
+    {
+        id: 13,
+        name: "24 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product4.png",
+        price: 2500
+    },
+    {
+        id: 14,
+        name: "40 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product4.png",
+        price: 3000
+    },
+    {
+        id: 15,
+        name: "40 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product4.png",
+        price: 3000
+    },
+    {
+        id: 16,
+        name: "40 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product4.png",
+        price: 3000
+    },
+    {
+        id: 17,
+        name: "40 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product4.png",
+        price: 3000
+    },
+    {
+        id: 18,
+        name: "18 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product4.png",
+        price: 2500
+    },
+    {
+        id: 19,
+        name: "40 DL. Двухконтурный настенный ",
+        imageUrl: "../images/product3.png",
+        price: 3000
+    },
+    {
+        id: 20,
+        name: "25 DL. Двухконтурный настенный WIFI",
+        imageUrl: "../images/product3.png",
+        price: 1000
+    },
+    {
+        id: 21,
+        name: "30 DL. Двухконтурный настенный",
+        imageUrl: "../images/product3.png",
+        price: 3000
+    },
+    {
+        id: 22,
+        name: "24 DL. Двухконтурный настенный",
+        imageUrl: "../images/product3.png",
+        price: 2500
+    },
+    {
+        id: 23,
+        name: "18 DL. Двухконтурный настенный",
+        imageUrl: "../images/product3.png",
+        price: 1000
+    },
+    {
+        id: 24,
+        name: "30 DL. Двухконтурный настенный",
+        imageUrl: "../images/product3.png",
+        price: 3000
+    },
+    {
+        id: 25,
+        name: "35 DL. Двухконтурный настенный",
+        imageUrl: "../images/product3.png",
+        price: 3000
+    },
+    {
+        id: 26,
+        name: "40 DL. Двухконтурный настенный",
+        imageUrl: "../images/product3.png",
+        price: 5000
+    },
+    {
+        id: 27,
+        name: "30 DL. Двухконтурный настенный",
+        imageUrl: "../images/product3.png",
+        price: 3000
+    },
+    {
+        id: 28,
+        name: "24 DL. Двухконтурный настенный",
+        imageUrl: "../images/product1.png",
+        price: 2500
+    },
+    {
+        id: 29,
+        name: "28 DL. Двухконтурный настенный",
+        imageUrl: "../images/product1.png",
+        price: 2500
+    },
+    {
+        id: 30,
+        name: "30 DL. Двухконтурный настенный",
+        imageUrl: "../images/product1.png",
+        price: 3000
+    },
+    {
+        id: 31,
+        name: "24 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product1.png",
+        price: 3000
+    },
+    {
+        id: 32,
+        name: "18 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product1.png",
+        price: 2500
+    },
+    {
+        id: 33,
+        name: "13 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product1.png",
+        price: 1000
+    },
+    {
+        id: 34,
+        name: "10 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product1.png",
+        price: 1000
+    },
+    {
+        id: 35,
+        name: "24 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product1.png",
+        price: 2500
+    },
+    {
+        id: 36,
+        name: "18 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "https:../images/product5.png36.jpg",
+        price: 1000
+    },
+    {
+        id: 37,
+        name: "13 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product5.png",
+        price: 1000
+    },
+    {
+        id: 38,
+        name: "10 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product5.png",
+        price: 1000
+    },
+    {
+        id: 39,
+        name: "24 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product5.png",
+        price: 2500
+    },
+    {
+        id: 40,
+        name: "10 DP. Двухконтурный настенный  WIFI",
+        imageUrl: "../images/product5.png",
+        price: 1000
+    }
+];
+
